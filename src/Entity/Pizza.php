17d074f76311f547ec3b31165cc5858f80698f2e
Ttst <thibautstachnick@gmail.com>
2024-01-14 13:42:58 +0100

    change database from mysql to postgresql

--- src/Entity/Pizza.php
+++ src/Entity/Pizza.php
@@ -12,7 +12,45 @@ use Core\Attributes\TargetRepository;
 class Pizza
 {
 
-    private $id;
+    private int $id;
+    private string $name;
+    private string $description;
+    private int $price;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
 
 
 }
--- src/Repository/PizzaRepository.php
+++ src/Repository/PizzaRepository.php
@@ -4,9 +4,37 @@ namespace App\Repository;
 
 use App\Entity\Pizza;
 use Core\Attributes\TargetEntity;
+use Core\Repository\Repository;
 
 #[TargetEntity(name: Pizza::class)]
-class PizzaRepository
+class PizzaRepository extends Repository
 {
+
+    public function save(Pizza $pizza){
+
+        $query = $this->pdo->prepare("INSERT INTO $this->tableName SET name:name, description:description, degree:degree");
+
+        $query->execute(
+            [
+                "name"=>$pizza->getName(),
+                "description"=>$pizza->getDescription(),
+                "price"=>$pizza->getPrice(),
+            ]
+        );
+    }
+
+    public function edit(Pizza $pizza){
+
+        $query = $this->pdo->prepare("INSERT INTO $this->tableName SET name:name, description:description, degree:degree WHERE id:id");
+
+        $query->execute(
+            [
+                "id"=>$pizza->getId(),
+                "name"=>$pizza->getName(),
+                "description"=>$pizza->getDescription(),
+                "price"=>$pizza->getPrice(),
+            ]
+        );
+    }
 
 }

8114391c58dcfc93cea711d77e75419fc62016f8
Ttst <thibautstachnick@gmail.com>
2024-01-21 11:32:47 +0100

    add the renderQuack method

--- core/View/View.php
+++ core/View/View.php
@@ -4,32 +4,41 @@ namespace Core\View;
 
 class View
 {
-   public static function render($nomDeTemplate, $donnees): void
-   {
 
-        ob_start();
-        extract($donnees);
+      public static function render($nomDeTemplate, $donnees): void
+      {
 
-        require_once "../templates/$nomDeTemplate.html.php";
+      ob_start();
+      extract($donnees);
 
-        $content = ob_get_clean();
+      require_once "../templates/$nomDeTemplate.html.php";
 
-        if(!isset($pageTitle)){ $pageTitle = "Pas de titre"; }
+      $content = ob_get_clean();
 
-        ob_start();
-        require_once "../templates/base.html.php";
-        echo ob_get_clean();
+      if(!isset($pageTitle)){ $pageTitle = "Pas de titre"; }
 
-    }
+      ob_start();
+      require_once "../templates/base.html.php";
+      echo ob_get_clean();
 
-   /**
-    * function view($viewName,$data = []){
-    * $viewPath = __DIR__."/templates/";
-    * require $viewPath.$viewName.".html.noot";
-    * //idées retenues:flop, weed,🤡,dwayneJohnson, quack, noot
-    *
-    */
+      }
 
 
+     public static function renderQuack($viewName,$data = []): void
+     {
+         $viewPath = "../templates/";
+         $cachePath = "../cache/";
+         extract($data);
+         $content = file_get_contents($viewPath . $viewName . ".html.quack");
+         $content = preg_replace('/{{\s*(.+?)\s*}}/', '<?php echo $1; ?>', $content);
+         $cacheFile = $cachePath.$viewName.".html.quack";
+         $directoryName = strtok($viewName, '/');
+         mkdir($cachePath.$directoryName);
+         file_put_contents($cacheFile,$content);
+         $cacheFile = ob_get_clean();
+         require_once "../templates/base.html.php";
+         echo ob_get_clean();
+     }
 
 }
+//idées retenues:flop, weed,🤡,dwayneJohnson, quack, noot

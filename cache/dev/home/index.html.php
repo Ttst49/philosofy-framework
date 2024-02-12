03ff1eabd9340f60a769070dedfd95e1ade5e2b4
Ttst <thibautstachnick@gmail.com>
2024-02-12 11:37:37 +0100

    templates for testing purpose

--- cache/dev/home/index.html.php
+++ cache/dev/home/index.html.php
@@ -5,9 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title> <?= $title; ?> </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-primary">
@@ -31,17 +31,10 @@
 
 <div class="container mt-5">
 
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
-
-
     <h2>Bienvenue dans le framework</h2>
     <p>Bonjour <?= $name; ?></p>
     <?php foreach($fruits as $fruit): ?>
-        <p><?= $fruit; ?></p>
+        <p>Le fruit est <?= $fruit; ?></p>
     <?php endforeach ?>
 
     <form method="post" action="#">
@@ -56,3 +49,12 @@
             OK CHEF
         </button>
     </form>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+

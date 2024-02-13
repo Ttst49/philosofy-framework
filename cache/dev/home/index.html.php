c609e7098db2e70c91cddc7108292d8b017dd276
Ttst <thibautstachnick@gmail.com>
2024-02-13 18:19:29 +0100

    update index into a working one

--- cache/dev/home/index.html.php
+++ cache/dev/home/index.html.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> <?= $title; ?> </title>
+    <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="">
 </head>
@@ -31,6 +31,15 @@
 
 <div class="container mt-5">
 
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
+
+<?php block title ?> <?= $title; ?> <?php endblock ?>
+
+<?php block content ?>
     <h2>Bienvenue dans le framework</h2>
     <p>Bonjour <?= $name; ?></p>
     <?php foreach($fruits as $fruit): ?>
@@ -49,12 +58,4 @@
             OK CHEF
         </button>
     </form>
-
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
-
-
-
+<?php endblock ?>

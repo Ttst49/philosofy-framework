6bd40dec3f85f5eaa9604c8fe5a66fdd78fdd588
Ttst <thibautstachnick@gmail.com>
2024-02-13 18:25:23 +0100

    comment methods that does not work

--- core/Quack/Quack.php
+++ core/Quack/Quack.php
@@ -76,66 +76,68 @@ class Quack
     {
 
           preg_match_all('/{% ?block ?(.*?) ?%}(.*?){% ?endblock ?%}/is', $content, $matches, PREG_SET_ORDER);
-        /**
-         * foreach ($matches as $value) {
-         * if (!array_key_exists($value[1], self::$blocks)) self::$blocks[$value[1]] = '';
-         * if (!str_contains($value[2], '@parent')) {
-         * self::$blocks[$value[1]] = $value[2];
-         * } else {
-         * self::$blocks[$value[1]] = str_replace('@parent', self::$blocks[$value[1]], $value[2]);
-         * }
-         * $content = str_replace($value[0], '', $content);
-         * }
-         */
+
+         foreach ($matches as $value) {
+         if (!array_key_exists($value[1], self::$blocks)) self::$blocks[$value[1]] = '';
+         if (!str_contains($value[2], '@parent')) {
+         self::$blocks[$value[1]] = $value[2];
+         } else {
+         self::$blocks[$value[1]] = str_replace('@parent', self::$blocks[$value[1]], $value[2]);
+         }
+         $content = str_replace($value[0], '', $content);
+         }
+
 
 
           //self::compileBlockContent($content);
-          self::compileBlockTitle($content);
+          //self::compileBlockTitle($content);
 
         return $content;
     }
 
-    static function compileBlockContent($content){
-        preg_match_all('/{% ?block content ?%}(.*?){% ?endblock ?%}/is', $content, $matches, PREG_SET_ORDER);
-
-        foreach ($matches as $value) {
-                if (!array_key_exists($value[1], self::$blocks)) self::$blocks[$value[1]] = '';
-                if (!str_contains($value[2], '@parent')) {
-                    self::$blocks[$value[1]] = $value[2];
-                } else {
-                    self::$blocks[$value[1]] = str_replace('@parent', self::$blocks[$value[1]], $value[2]);
-                }
-                $content = str_replace($value[0], '', $content);
-        }
-        return $content;
-    }
-
-    //registre des noms de blocks autorisés
-
-    static function compileBlockTitle($content){
-        preg_match_all('/{% ?block title ?%}(.*?){% ?endblock ?%}/is', $content, $matches, PREG_SET_ORDER);
-
-        foreach ($matches as $value) {
-            if (!array_key_exists($value[1], self::$blocks)) self::$blocks[$value[1]] = '';
-            if (!str_contains($value[2], '@parent')) {
-                self::$blocks[$value[1]] = $value[2];
-            } else {
-                self::$blocks[$value[1]] = str_replace('@parent', self::$blocks[$value[1]], $value[2]);
-            }
-            $content = str_replace($value[0], '', $content);
-        }
-        return $content; preg_match_all('/{% ?block title ?%}(.*?){% ?endblock ?%}/is', $content, $matches, PREG_SET_ORDER);
-        foreach ($matches as $value) {
-                if (!array_key_exists($value[1], self::$blocks)) self::$blocks[$value[1]] = '';
-                if (!str_contains($value[2], '@parent')) {
-                    self::$blocks[$value[1]] = $value[2];
-                } else {
-                    self::$blocks[$value[1]] = str_replace('@parent', self::$blocks[$value[1]], $value[2]);
-                }
-                $content = str_replace($value[0], '', $content);
-        }
-        return $content;
-    }
+    /**
+     * function compileBlockContent($content){
+     * preg_match_all('/{% ?block content ?%}(.*?){% ?endblock ?%}/is', $content, $matches, PREG_SET_ORDER);
+     *
+     * foreach ($matches as $value) {
+     * if (!array_key_exists($value[1], self::$blocks)) self::$blocks[$value[1]] = '';
+     * if (!str_contains($value[2], '@parent')) {
+     * self::$blocks[$value[1]] = $value[2];
+     * } else {
+     * self::$blocks[$value[1]] = str_replace('@parent', self::$blocks[$value[1]], $value[2]);
+     * }
+     * $content = str_replace($value[0], '', $content);
+     * }
+     * return $content;
+     * }
+     *
+     * //registre des noms de blocks autorisés
+     *
+     * static function compileBlockTitle($content){
+     * preg_match_all('/{% ?block title ?%}(.*?){% ?endblock ?%}/is', $content, $matches, PREG_SET_ORDER);
+     *
+     * foreach ($matches as $value) {
+     * if (!array_key_exists($value[1], self::$blocks)) self::$blocks[$value[1]] = '';
+     * if (!str_contains($value[2], '@parent')) {
+     * self::$blocks[$value[1]] = $value[2];
+     * } else {
+     * self::$blocks[$value[1]] = str_replace('@parent', self::$blocks[$value[1]], $value[2]);
+     * }
+     * $content = str_replace($value[0], '', $content);
+     * }
+     * return $content; preg_match_all('/{% ?block title ?%}(.*?){% ?endblock ?%}/is', $content, $matches, PREG_SET_ORDER);
+     * foreach ($matches as $value) {
+     * if (!array_key_exists($value[1], self::$blocks)) self::$blocks[$value[1]] = '';
+     * if (!str_contains($value[2], '@parent')) {
+     * self::$blocks[$value[1]] = $value[2];
+     * } else {
+     * self::$blocks[$value[1]] = str_replace('@parent', self::$blocks[$value[1]], $value[2]);
+     * }
+     * $content = str_replace($value[0], '', $content);
+     * }
+     * return $content;
+     * }
+     */
 
     static function compileYield($content): array|string|null
     {

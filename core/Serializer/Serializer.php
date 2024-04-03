166e8334afcbf50a4dc63e6911aa42d96a5a3ad3
Ttst <thibautstachnick@gmail.com>
2024-04-03 12:46:46 +0200

    add deserialize

--- core/Serializer/Serializer.php
+++ core/Serializer/Serializer.php
@@ -4,12 +4,24 @@ namespace Core\Serializer;
 
 class Serializer
 {
-    public static function serialize($toSerialize)
-    {
-//        $serialized = json_encode($toSerialize);
-        //status http
-        //interfae sérializable
-        //serializable sur entité direct
-        echo (json_encode($toSerialize));
+    public static function deserialize($toDeserialize, $type = null /**string $format*/){
+        if ($type){
+            if (class_exists($type)){
+                $deserialized = json_decode(strtolower($toDeserialize));
+                $entity = new $type;
+                $classType = new \ReflectionClass($type);
+                $methods = $classType->getMethods();
+                foreach ($methods as $method){
+                    if (strtolower(substr($method->getName(), 0, 3)) === "set"){
+                        $tmpMeth = $method->getName();
+                        $supposedProperty = substr(strtolower($method->getName()), 3);
+                        $entity->$tmpMeth($deserialized->$supposedProperty);
+                    }
+                }
+                return $entity;
+            }else return "class does not exist";
+        }else{
+            return json_decode($toDeserialize, true);
+        }
     }
 }
